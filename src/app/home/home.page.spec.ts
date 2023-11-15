44b6583de5a00f419d6a38265e7eb50a6b178ac9
Javier Arenas <javier.arenas@vipera.com>
2023-11-15 14:55:01 +0100

    feat: Responsive layout, Validation, Icons and Unit tests

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,12 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import {HomePage} from './home.page';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {By} from "@angular/platform-browser";
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+      ],
+    })
+      .compileComponents();
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -15,4 +23,19 @@ describe('HomePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should require a valid email', () => {
+    component.login.setValue({
+      email: 'invalid',
+      password: 'password',
+      remember: false
+    })
+    expect(component.login.get('email')?.valid).toBeFalse()
+    component.login.setValue({
+      email: 'valid@valid.com',
+      password: 'password',
+      remember: false
+    })
+    expect(component.login.get('email')?.valid).toBeTruthy()
+  })
 });
